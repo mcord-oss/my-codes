f8c4377a94ff33a65b8f46d18ba3ead1bf05b07a
mcord-oss <ordonjulius1@gmail.com>
2025-05-01 15:29:10 +0800

    php data object

--- index.php
+++ index.php
@@ -1,71 +1,122 @@
 <?php
-if(!isset($_SESSION)){
-    session_start();
-}
+    
+    // creating a variables
+            $host = "localhost";
+            $user = "root";
+            $pass = "admin";
+            $db = "dbname";
+    // data source name
+            $dns = "mysql:host=$host;dbname=$db";
+    // pdo instanciation
+            $pdo = new PDO($dns, $user, $pass);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+    // query
+            $stmt = $pdo->query( "SELECT * FROM `employees`");
+    // loop
+            while($row = $stmt->fetch()){
+                echo $row['first_name']." ".$row['last_name']." ".$row['email']."<br/>";
+            }
 
-if(isset($_SESSION['userlogin'])){
-    echo "WELCOME ".$_SESSION['userlogin'];
-}else{
-    echo "WELCOME GUEST!!";
-}
+            while($row = $stmt->fetch(PDO::FETCH_OBJ)){
+                echo $row->first_name." ".$row->gender."<br/>";
+            }
 
-    include_once("connections/connection.php");
-    $con = connection();
+            foreach($stmts as $stmt){
+                echo $stmt->first_name." ".$stmt->email."<br/>";
+            }
 
-    $sql = "SELECT * FROM `employees` ORDER BY id DESC";
-    $stmt = $con->query($sql) or die ($con->error);
-    $row = $stmt->fetch_assoc();
+    // prepared statements 
+    // positional parameter
+            $gender = "male";
+
+            $sql = "SELECT * FROM `employees` WHERE `gender` = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$gender]);
+            $users = $stmt->fetchAll();
+
+            foreach($users as $user){
+                echo $user->first_name." ".$user->last_name." ".$user->email."<br/>";
+            }
+
+    // name parameter
+
+            $sql = "SELECT * FROM `employees` WHERE `gender` = :gender";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['gender'=>$gender]);
+            $users = $stmt->fetchAll();
+
+            foreach($users as $user){
+                echo $user['first_name'].$user['email'].$user['gender']."<br/>";
+            }
+
+    // getting single record
+            $id = 3;
+
+            $sql = "SELECT * FROM `employees` WHERE `id` = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$id]);
+            $user = $stmt->fetch();
+
+            echo $user['first_name']." ".$user['last_name'].$user['email'].$user['gender'];
+
+    // getting a number of rows
+
+            $sql = "SELECT * FROM `employees` WHERE `gender` = :gender";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['gender'=>$gender]);
+            $userCount = $stmt->rowCount();
+            echo $userCount;
+
+    // insert datas
+
+            $fname = "JUAN";
+            $lname = "DELA CRUZ";
+            $email = "JUANDELACRUZ1@PDO.org";
+            $gender = "MALE";
+
+            $sql = "INSERT INTO `employees`(`first_name`,`last_name`,`email`,`gender`)VALUES(?, ?, ?, ?)";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$fname, $lname, $email, $gender]);
+            $users = $stmt->fetchAll();
+            $userCount = $stmt->rowCount();
+
+    // update data
+            
+            $fname = "JEAN";
+            $lname = "DELA CRUZ";
+            $email = "JEANNA22@GMAIL.COM";
+            $gender = "FEMALE";
+            $id = 1066;
+
+            $sql = "UPDATE `employees` SET `first_name` = :first_name, `email` = :email, `gender` = :gender WHERE `id` = :id";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute(['first_name'=>$fname, 'email'=>$email, 'gender'=>$gender, 'id'=>$id]);
+            $users = $stmt->fetchAll();
+            $userCount = $stmt->rowCount();
+
+    // delete data
+
+            $id = 1067;
+
+            $sql = "DELETE FROM `employees` WHERE `id` = ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$id]);
+            $users = $stmt->fetchAll();
+            $userCount = $stmt->rowCount();
+
+    // search datas
+
+            $search = "%female%";
+
+            $sql = "SELECT * FROM `employees` WHERE `gender` LIKE ?";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$search]);
+            $users = $stmt->fetchAll();
+            $userCount = $stmt->rowCount();
+
+            foreach($users as $user){
+                echo $user['first_name']." ".$user['last_name']." ".$user['email']." ".$user['gender']."<br/>";
+            }
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body><br><br>
-<?php if(isset($_SESSION['userlogin'])){?>
-        <a href="logout.php">Log-out</a>
-    <?php } else { ?>
-        <a href="login.php">Log-in</a>
-    <?php } ?>
-    <h2 style="text-align: center;">System Information</h2>
-    <form action="result.php" method="get" class="myForm">
-        <input type="search" name="query" id="query" placeholder="Type Here">
-        <button type="search" name="btn">Search</button>
-    </form>
-    <table class="table table-bordered table-hover table-frimary">
-        <thead class="table-success">
-            <tr>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php while($row = $stmt->fetch_assoc()){?>
-            <tr>
-                <td><?=$row['id'];?>.</td>
-                <td><?=$row['first_name'];?></td>
-                <td><?=$row['last_name'];?></td>
-                <td><?=$row['email'];?></td>
-                <td><?=$row['gender'];?></td>
-                <td><a href="details.php?id=<?php echo $row['id'];?>">view</a></td>
-                <td>
-                    <img width="100" height="50" src="images/<?php echo $row['img'];?>" alt="image">
-                </td>
-            </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
